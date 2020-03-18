a63753e838fea271c4ebfce602b68efdadc07235
MatthewJOwens <mjowens@mail.com>
2020-03-18 15:54:41 -0600

    Fully functional and looks good

--- app.js
+++ app.js
@@ -10,14 +10,24 @@ let scissors = {
   img: "./RPSScissors.png",
   text: "SCISSORS"
 }
+let win = {
+  text: "You win!"
+}
+let lose = {
+  text: "You lose."
+}
+let draw = {
+  text: "Draw! Play again!"
+}
 
 function play(userInput) {
   console.log("Plays " + userInput.text.toLowerCase());
   // @ts-ignore
   document.getElementById("player-img").src = userInput.img
   document.getElementById("player-plays").innerText = userInput.text
-  let fight = opponentPlay()
-  eval(userInput, fight)
+  let opponentInput = opponentPlay()
+  let fight = eval(userInput, opponentInput)
+  setTimeout(results, 500, fight)
 }
 
 function opponentPlay() {
@@ -48,5 +58,36 @@ function opponentPlay() {
 
 function eval(user, opponent) {
   console.log(user + " vs. " + opponent)
+
+  switch (user) {
+    case rock:
+      if (opponent == rock) {
+        return draw;
+      } else if (opponent == paper) {
+        return lose;
+      } else {
+        return win;
+      }
+    case paper:
+      if (opponent == paper) {
+        return draw;
+      } else if (opponent == scissors) {
+        return lose;
+      } else {
+        return win;
+      }
+    case scissors:
+      if (opponent == scissors) {
+        return draw;
+      } else if (opponent == rock) {
+        return lose;
+      } else {
+        return win;
+      }
+  }
+}
+
+function results(fight) {
+  alert(fight.text);
 
 }
